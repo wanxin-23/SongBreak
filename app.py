69bd4f4b93b0316f6a825166446ba232bd1e6e56
wanxin-23 <128222908+wanxin-23@users.noreply.github.com>
2024-03-01 00:38:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,19 +10,38 @@ sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
 app = Flask(__name__)
     
+def remove_bracket(s):
+    bracket_index = s.find('(')
+    if bracket_index != -1:
+        return s[:bracket_index] 
+    return s  
+    
+def search(query):
+    offset_value = 0
+    track_list = []
+    while offset_value <= 900:
+        results = sp.search(q=query, limit=50, offset=offset_value)
+        for track in results['tracks']['items']:
+            track_info = {'name': track['name'], 
+                          'artist': track['artists'][0]['name']}
+            if (track_info['name'].lower() == query.lower()):
+                return track_info
+            else:
+                break
+        offset_value += 50
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         query = request.form['query']
-        results = sp.search(q=query, type='track', limit=10)
+        words = query.split(" ")
         tracks = []
-        for track in results['tracks']['items']:
-            track_info = {
-                'name': track['name'],
-                'artist': track['artists'][0]['name']}
-            tracks.append(track_info)
+        for word in words: 
+            results = search(word)
+            tracks.append(results)
         return render_template('results.html', tracks=tracks, query=query)
     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
